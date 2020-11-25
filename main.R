6451928520c78c5615ca4548aad136edb8047095
DivyaNidadavolu26 <34182120+DivyaNidadavolu26@users.noreply.github.com>
2020-11-25 11:50:50 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,2 +1,20 @@
 ## Loading required libraries
 library(ggplot2)
+library(tidytext)
+library(rtweet)
+library(twitteR)
+library(ROAuth)
+library(RCurl)
+library(purr)
+library(stringr)
+library(dplyr)
+library(plyr)
+library(openssl)
+library(httpuv)
+
+## Downloading the data for positive and negative words
+positive_words <- scan(file = "positive-words.txt", what = "character")
+negative_words <- scan(file = "negative-words.txt", what = "character")
+
+## Cleaning the data
+## Removing punctuations
